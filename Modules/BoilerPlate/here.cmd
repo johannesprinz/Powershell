5111b6e934ef3f6b255bf86e3f12c6547fd78910
jprinz <jprinz@boardbooks.com>
2014-10-28 09:12:10 +1300

    Added package restore function

--- Modules/BoilerPlate/here.cmd
+++ Modules/BoilerPlate/here.cmd
@@ -1,2 +1,3 @@
 @echo off
+"..\..\.nuget\NuGet.exe" restore -PackagesDirectory "..\..\packages"
 powershell -NoLogo -NoExit -ExecutionPolicy Bypass -Command "& '%~dp0\..\..\packages\psake.4.3.2\tools\psake.ps1' -docs %*;"
--- Modules/BoilerPlate/pester.cmd
+++ Modules/BoilerPlate/pester.cmd
@@ -1,2 +1,3 @@
 @echo off
+"..\..\.nuget\NuGet.exe" restore -PackagesDirectory "..\..\packages"
 powershell -NoLogo -NoExit -ExecutionPolicy Bypass -Command "& Import-Module '%~dp0\..\..\packages\Pester.3.1\tools\Pester.psm1'; & { Invoke-Pester -OutputFile 'bin\Test.xml' -OutputFormat LegacyNUnitXml -EnableExit %ARGS%}"
