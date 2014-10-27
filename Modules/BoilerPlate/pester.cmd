a2534b8955b12184e5b94b1e06e0929845083049
johannesprinz <johannesprinz@gmail.com>
2014-10-28 08:13:01 +1300

    Update pester.cmd

--- Modules/BoilerPlate/pester.cmd
+++ Modules/BoilerPlate/pester.cmd
@@ -1,2 +1,2 @@
 @echo off
-powershell -NoLogo -NoExit -ExecutionPolicy Bypass -Command "& Import-Module '%~dp0\..\Pester\Pester.psm1'; & { Invoke-Pester -OutputXml 'bin\Test.xml' -EnableExit %ARGS%}"
+powershell -NoLogo -NoExit -ExecutionPolicy Bypass -Command "& Import-Module '%~dp0\..\packages\Pester.3.1\tools\Pester.psm1'; & { Invoke-Pester -OutputXml 'bin\Test.xml' -EnableExit %ARGS%}"
